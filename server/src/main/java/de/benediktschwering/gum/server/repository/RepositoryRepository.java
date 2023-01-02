8853f0c870a34ddaed0a07fc81911239841fd00e
Benedikt <mail@benedikt-schwering.de>
2023-01-02 23:45:30 +0100

    implemented repository controller

--- server/src/main/java/de/benediktschwering/gum/server/repository/RepositoryRepository.java
+++ server/src/main/java/de/benediktschwering/gum/server/repository/RepositoryRepository.java
@@ -3,5 +3,10 @@ package de.benediktschwering.gum.server.repository;
 import de.benediktschwering.gum.server.model.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface RepositoryRepository extends MongoRepository<Repository, String> {
+
+    Optional<Repository> searchRepositoryByName(String name);
+
 }
